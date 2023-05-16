c5f8591eb65ae468b530ad47dd3d07e4f7debd24
Joey Kamping <73405764+ykzird@users.noreply.github.com>
2023-05-16 20:17:03 +0200

    Delete installscripts\install-choco.ps1

--- installscripts\install-choco.ps1
+++
@@ -1,1 +0,0 @@
-[system.net.servicepointmanager]::securityprotocol = [system.net.servicepointmanager]::securityprotocol -bor 3072; iex ((new-object system.net.webclient).downloadstring('https://community.chocolatey.org/install.ps1'));
